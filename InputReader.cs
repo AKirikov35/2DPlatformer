2548c9b0b01063f95f1590d935ce44f55e988372
AKirikov35 <161311981+AKirikov35@users.noreply.github.com>
2025-02-20 11:14:24 +0300

    Delete InputReader.cs

--- InputReader.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-
-public static class InputReader
-{
-    public const string Horizontal = nameof(Horizontal);
-    public const KeyCode KeyJump = KeyCode.Space;
-
-    public static bool IsMoveInput() => Input.GetAxis(Horizontal) != 0;
-}
